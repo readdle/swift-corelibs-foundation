099b442edc21f761aa82c0de07bf1d62f1f338ab
Andrew Druk <adruk@readdle.com>
2018-11-28 23:04:25 +0200

    Dev: fix OperationQueue

--- TestFoundation/TestOperationQueue.swift
+++ TestFoundation/TestOperationQueue.swift
@@ -25,6 +25,16 @@ class TestOperationQueue : XCTestCase {
             ("test_isSuspended", test_isSuspended),
             ("test_cancelDependency", test_cancelDependency),
             ("test_deadlock", test_deadlock),
+            ("test_cancel_out_of_queue", test_cancel_out_of_queue),
+            ("test_cross_queues_dependency", test_cross_queues_dependency),
+            ("test_suspended", test_suspended),
+            ("test_suspended2", test_suspended2),
+            ("test_operations_order", test_operations_order),
+            ("test_operations_order2", test_operations_order2),
+            ("test_wait_until_finished", test_wait_until_finished),
+            ("test_wait_until_finished_operation", test_wait_until_finished_operation),
+            ("test_custom_ready_operation", test_custom_ready_operation),
+            ("test_mac_os_10_6_cancel_behavour", test_mac_os_10_6_cancel_behavour),
         ]
     }
     
@@ -240,6 +250,283 @@ class TestOperationQueue : XCTestCase {
         queue.addOperation(op1)
         queue.addOperation(op2)
 
+        waitForExpectations(timeout: 1)
+    }
+    
+    public func test_cancel_out_of_queue() {
+        let op = Operation()
+        op.cancel()
+        
+        XCTAssert(op.isCancelled)
+        XCTAssertFalse(op.isExecuting)
+        XCTAssertFalse(op.isFinished)
+    }
+    
+    public func test_cross_queues_dependency() {
+        let queue = OperationQueue()
+        let queue2 = OperationQueue()
+        
+        let expectation1 = self.expectation(description: "Operation should finish")
+        let expectation2 = self.expectation(description: "Operation should finish")
+        
+        let op1 = BlockOperation {
+            expectation1.fulfill()
+        }
+        op1.name = "op1"
+        
+        let op2 = BlockOperation {
+            expectation2.fulfill()
+        }
+        op2.name = "op2"
+        
+        op1.addDependency(op2)
+        
+        queue.addOperation(op1)
+        queue2.addOperation(op2)
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    public func test_suspended() {
+        let queue = OperationQueue()
+        queue.isSuspended = true
+        
+        let expectation1 = self.expectation(description: "Operation should finish")
+        let expectation2 = self.expectation(description: "Operation should finish")
+        
+        let op1 = BlockOperation {
+            expectation1.fulfill()
+        }
+        op1.name = "op1"
+        
+        let op2 = BlockOperation {
+            expectation2.fulfill()
+        }
+        op2.name = "op2"
+        
+        queue.addOperation(op1)
+        queue.addOperation(op2)
+        
+        DispatchQueue.global().async {
+            queue.isSuspended = false
+        }
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    public func test_suspended2() {
+        let queue = OperationQueue()
+        queue.isSuspended = true
+        
+        let op1 = BlockOperation {}
+        op1.name = "op1"
+        
+        let op2 = BlockOperation {}
+        op2.name = "op2"
+        
+        queue.addOperation(op1)
+        queue.addOperation(op2)
+        
+        op1.cancel()
+        op2.cancel()
+        
+        queue.isSuspended = false
+        queue.waitUntilAllOperationsAreFinished()
+        
+        XCTAssert(op1.isCancelled)
+        XCTAssertFalse(op1.isExecuting)
+        XCTAssert(op1.isFinished)
+        XCTAssert(op2.isCancelled)
+        XCTAssertFalse(op2.isExecuting)
+        XCTAssert(op2.isFinished)
+    }
+    
+    public func test_operations_order() {
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 1
+        queue.isSuspended = true
+        
+        var array = [Int]()
+        
+        let op1 = BlockOperation {
+            array.append(1)
+        }
+        op1.queuePriority = .normal
+        op1.name = "op1"
+        
+        let op2 = BlockOperation {
+            array.append(2)
+        }
+        op2.queuePriority = .normal
+        op2.name = "op2"
+        
+        let op3 = BlockOperation {
+            array.append(3)
+        }
+        op3.queuePriority = .normal
+        op3.name = "op3"
+        
+        let op4 = BlockOperation {
+            array.append(4)
+        }
+        op4.queuePriority = .normal
+        op4.name = "op4"
+        
+        let op5 = BlockOperation {
+            array.append(5)
+        }
+        op5.queuePriority = .normal
+        op5.name = "op5"
+        
+        queue.addOperation(op1)
+        queue.addOperation(op2)
+        queue.addOperation(op3)
+        queue.addOperation(op4)
+        queue.addOperation(op5)
+        
+        queue.isSuspended = false
+        queue.waitUntilAllOperationsAreFinished()
+        
+        XCTAssertEqual(array, [1, 2, 3, 4, 5])
+    }
+    
+    public func test_operations_order2() {
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 1
+        queue.isSuspended = true
+        
+        var array = [Int]()
+        
+        let op1 = BlockOperation {
+            array.append(1)
+        }
+        op1.queuePriority = .veryLow
+        op1.name = "op1"
+        
+        let op2 = BlockOperation {
+            array.append(2)
+        }
+        op2.queuePriority = .low
+        op2.name = "op2"
+        
+        let op3 = BlockOperation {
+            array.append(3)
+        }
+        op3.queuePriority = .normal
+        op3.name = "op3"
+        
+        let op4 = BlockOperation {
+            array.append(4)
+        }
+        op4.queuePriority = .high
+        op4.name = "op4"
+        
+        let op5 = BlockOperation {
+            array.append(5)
+        }
+        op5.queuePriority = .veryHigh
+        op5.name = "op5"
+        
+        queue.addOperation(op1)
+        queue.addOperation(op2)
+        queue.addOperation(op3)
+        queue.addOperation(op4)
+        queue.addOperation(op5)
+        
+        queue.isSuspended = false
+        queue.waitUntilAllOperationsAreFinished()
+        
+        XCTAssertEqual(array, [5, 4, 3, 2, 1])
+    }
+    
+    func test_wait_until_finished() {
+        let queue1 = OperationQueue()
+        let queue2 = OperationQueue()
+        
+        let op1 = BlockOperation { sleep(1) }
+        let op2 = BlockOperation { }
+        
+        op2.addDependency(op1)
+        
+        queue1.addOperation(op1)
+        queue2.addOperation(op2)
+        
+        queue2.waitUntilAllOperationsAreFinished()
+        XCTAssertEqual(queue2.operationCount, 0)
+    }
+    
+    func test_wait_until_finished_operation() {
+        let queue1 = OperationQueue()
+        let op1 = BlockOperation { sleep(1) }
+        queue1.addOperation(op1)
+        op1.waitUntilFinished()
+        XCTAssertEqual(queue1.operationCount, 0)
+    }
+    
+    func test_custom_ready_operation() {
+        class CustomOperation: Operation {
+            
+            private var _isReady = false
+            
+            override var isReady: Bool {
+                return _isReady
+            }
+            
+            func setIsReady() {
+                willChangeValue(forKey: "isReady")
+                _isReady = true
+                didChangeValue(forKey: "isReady")
+            }
+            
+        }
+        
+        let expectation = self.expectation(description: "Operation should finish")
+        
+        let queue1 = OperationQueue()
+        let op1 = CustomOperation()
+        let op2 = BlockOperation(block: {
+            expectation.fulfill()
+        })
+        
+        queue1.addOperation(op1)
+        queue1.addOperation(op2)
+        
+        waitForExpectations(timeout: 1)
+        
+        XCTAssertEqual(queue1.operationCount, 1)
+        op1.setIsReady()
+        queue1.waitUntilAllOperationsAreFinished()
+        XCTAssertEqual(queue1.operationCount, 0)
+    }
+    
+    func test_mac_os_10_6_cancel_behavour() {
+        
+        let expectation1 = self.expectation(description: "Operation should finish")
+        let expectation2 = self.expectation(description: "Operation should finish")
+        
+        let queue1 = OperationQueue()
+        let op1 = BlockOperation(block: {
+            expectation1.fulfill()
+        })
+        let op2 = BlockOperation(block: {
+            expectation2.fulfill()
+        })
+        let op3 = BlockOperation(block: {
+            // empty
+        })
+        
+        op1.addDependency(op2)
+        op2.addDependency(op3)
+        op3.addDependency(op1)
+        
+        queue1.addOperation(op1)
+        queue1.addOperation(op2)
+        queue1.addOperation(op3)
+        
+        XCTAssertEqual(queue1.operationCount, 3)
+        
+        op3.cancel()
+        
         waitForExpectations(timeout: 1)
     }
 }
