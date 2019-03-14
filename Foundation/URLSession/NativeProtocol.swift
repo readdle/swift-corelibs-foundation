a4266743cb1ccb69464da2096f7da7a7435336b5
Albert Aleksieiev <albert.aleksieiev@gmail.com>
2019-03-14 14:04:14 +0200

    Fix: avoid fatalError if transfer is completed without response for URLSession

--- Foundation/URLSession/NativeProtocol.swift
+++ Foundation/URLSession/NativeProtocol.swift
@@ -207,8 +207,14 @@ internal class _NativeProtocol: URLProtocol, _EasyHandleDelegate {
         }
 
         guard let response = ts.response else {
-            fatalError("Transfer completed, but there's no response.")
+            // This is a workaround, not a fix. Instead of fatalError, just failing current transfer.
+            // fatalError("Transfer completed, but there's no response.")
+            NSLog("Transfer completed, but there's no response. Transfer is failed.")
+            internalState = .transferFailed
+            failWith(error: NSError(domain: NSURLErrorDomain, code: NSURLErrorUnknown), request: request)
+            return
         }
+        
         internalState = .transferCompleted(response: response, bodyDataDrain: ts.bodyDataDrain)
         let action = completionAction(forCompletedRequest: request, response: response)
 
