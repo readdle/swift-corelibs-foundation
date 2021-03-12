49f0e7c2b013efe54838ef75b6f56b785f7f7356
Kostiantyn Gominyuk <kgominyuk@readdle.com>
2021-03-12 09:50:12 +0200

    Add ability to trust invalid certificate
    Co-authored-by: Andrew Druk <adruk@readdle.com>
    Co-authored-by: Albert Aleksieiev <albert.aleksieiev@gmail.com>

--- Sources/FoundationNetworking/URLSession/URLSessionTask.swift
+++ Sources/FoundationNetworking/URLSession/URLSessionTask.swift
@@ -968,6 +968,19 @@ extension _ProtocolClient : URLProtocolClient {
 
     func urlProtocol(_ protocol: URLProtocol, didFailWithError error: Error) {
         guard let task = `protocol`.task else { fatalError() }
+        
+        if error._code == NSURLErrorServerCertificateUntrusted {
+            let protectionSpace = URLProtectionSpace(host: "", port: 443, protocol: "https", realm: "",
+                                                     authenticationMethod: NSURLAuthenticationMethodServerTrust)
+
+            let authenticationChallenge = URLAuthenticationChallenge(protectionSpace: protectionSpace, proposedCredential: nil,
+                                                                     previousFailureCount: task.previousFailureCount, failureResponse: nil, error: error,
+                                                                     sender: URLSessionAuthenticationChallengeSender())
+            task.previousFailureCount += 1
+            urlProtocol(`protocol`, didReceive: authenticationChallenge)
+            return
+        }
+
         urlProtocol(task: task, didFailWithError: error)
     }
 
@@ -1073,6 +1086,7 @@ extension URLSessionTask {
             NSURLAuthenticationMethodHTTPBasic : basicAuth,
             NSURLAuthenticationMethodHTTPDigest: digestAuth,
             NSURLAuthenticationMethodNTLM      : ntlmAuth,
+            NSURLAuthenticationMethodServerTrust: serverTrustAuth,
         ]
         return handlers[authScheme]
     }
@@ -1095,6 +1109,11 @@ extension URLSessionTask {
         task.authRequest = task.originalRequest
         task.authRequest?.authMethod = NSURLAuthenticationMethodNTLM
         task.authRequest?.credential = credential
+    }
+
+    static func serverTrustAuth(_ task: URLSessionTask, _ disposition: URLSession.AuthChallengeDisposition, _ credential: URLCredential?) {
+        task.authRequest = task.originalRequest
+        task.authRequest?.trustAllCertificates = credential?._trustAllCertificates
     }
 
 }
