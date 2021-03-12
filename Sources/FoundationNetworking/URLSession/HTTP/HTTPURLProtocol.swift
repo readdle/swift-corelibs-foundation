c5318d7ed664e9b118303a3c211c1514c60dbb57
Kostiantyn Gominyuk <kgominyuk@readdle.com>
2021-03-12 09:50:12 +0200

    Add ability to trust invalid certificate
    Co-authored-by: Andrew Druk <adruk@readdle.com>
    Co-authored-by: Albert Aleksieiev <albert.aleksieiev@gmail.com>

--- Sources/FoundationNetworking/URLSession/HTTP/HTTPURLProtocol.swift
+++ Sources/FoundationNetworking/URLSession/HTTP/HTTPURLProtocol.swift
@@ -443,6 +443,10 @@ internal class _HTTPURLProtocol: _NativeProtocol {
                 easyHandle.set(username: username, password: password)
             }
         }
+
+        if let trustAllCertificates = request.trustAllCertificates {
+            easyHandle.set(trustAllCertificates: trustAllCertificates)
+        }
     }
 
     /// What action to take
